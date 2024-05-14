fda1f0bdac36d59c4bee561d05cbc65b6dae59f3
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-05-15 00:44:59 +0200

    Added dialogs for editing given user info

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -5,6 +5,10 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '../../interfaces/user.interface';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu'
+import { DialogEditUserComponent } from './dialog-edit-user/dialog-edit-user.component';
+import { DialogEditAddressComponent } from './dialog-edit-address/dialog-edit-address.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,7 +16,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
   ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
@@ -20,6 +25,7 @@ import { MatButtonModule } from '@angular/material/button';
 export class UserDetailComponent implements OnInit {
   firestore: Firestore = inject(Firestore);
   route: ActivatedRoute = inject(ActivatedRoute);
+  dialog: MatDialog = inject(MatDialog);
 
   userId = '';
   user: User | null = null;
@@ -35,7 +41,19 @@ export class UserDetailComponent implements OnInit {
     });
   }
 
-  openAddressDialog() {
+  openUserEditor() {
+    const dialogRef = this.dialog.open(DialogEditUserComponent);
 
+    dialogRef.afterClosed().subscribe(async (result: any) => {
+
+    });
+  }
+
+  openAddressEditor() {
+    const dialogRef = this.dialog.open(DialogEditAddressComponent);
+
+    dialogRef.afterClosed().subscribe(async (result: any) => {
+
+    });
   }
 }
