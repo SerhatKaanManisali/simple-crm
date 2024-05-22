62a09276d77b399b309f73f007240e47e3abbce6
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-05-22 05:45:08 +0200

    Added product page and fixed bug regarding icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
     { path: 'user/:id', component: UserDetailComponent },
     { path: 'user', component: UserComponent },
+    { path: 'product', component: ProductComponent },
     { path: '', component: DashboardComponent }
 ];
